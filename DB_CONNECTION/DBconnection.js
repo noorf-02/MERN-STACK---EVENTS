dea08859a39a3b074429d95a92db632a1954c09e
Noor Fatima <noor2239064@gmail.com>
2026-09-04 23:10:23 +0500

    changes

--- DB_CONNECTION/DBconnection.js
+++ DB_CONNECTION/DBconnection.js
@@ -4,7 +4,7 @@ const connectDB = async (req,res)=>{
     await mongoose.connect(process.env.URI).then(()=>{
         console.log('DB is connected. Keep working')
     }).catch(err=>{
-        console.log('Error in DB connection');
+        console.log('Error in DB connection', err);
     })
 };
 
